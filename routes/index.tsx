177bb189ca394420981a0a0eb2f6ec7522dcde97
kiri23 <christian@opticpower.com>
2024-05-28 20:42:16 -0400

    hide navbar to test github action

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,13 @@
 // routes/index.tsx
+// /** @jsx h */ this lime remove the red lines in the editor
 import { h } from "preact";
-import Navbar from "../components/Navbar.tsx";
+// import Navbar from "../components/Navbar.tsx";
 import { HomeSection, ValuesSection, ServicesSection, MeetTherapistSection } from "../components/Sections/index.tsx";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <Navbar />
+      {/* <Navbar /> */}
       <HomeSection />
       <ValuesSection />
       <ServicesSection />
