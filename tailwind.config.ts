2c2e907786be9faf2b782a875e61c190ed404437
kiri23 <christian@opticpower.com>
2024-07-09 16:32:12 -0400

    remove daisy ui mess with tailwind intelisense

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,24 @@
 import { type Config } from "tailwindcss";
-import daisyui from "npm:daisyui";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
+    "node_modules/preline/dist/*.js",
   ],
   theme: {
     colors: {
       pink: "#f5dae1",
+      blue: "#2B4257",
+      white: "#ffffff",
+    },
+    extend: {
+      backgroundImage: {
+        "hero-person": "url('heroPerson')",
+      },
     },
     container: {
       center: true,
     },
   },
-  // @ts-ignore
-  plugins: [daisyui],
-  daisyui: {
-    themes: ["light"], // or any other themes you want to use
-  },
+  // not using any plugins because it mess with vs code tailwind intelissense.
 } satisfies Config;
