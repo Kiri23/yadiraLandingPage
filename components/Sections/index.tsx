3a1b62c55b70beed805b413f67cdfac8a7b3717f
kiri23 <christian@opticpower.com>
2024-07-10 13:26:21 -0400

    create second version

--- components/Sections/index.tsx
+++ components/Sections/index.tsx
@@ -2,4 +2,5 @@
 export { default as HeroSection } from "./Hero.tsx";
 export { default as HeroSection2 } from "./Hero2.tsx";
 export { default as ServicesSection } from "./Services.tsx";
+export { default as ServicesSection2 } from "./Services2.tsx";
 export { default as MeetTherapistSection } from "./MeetMe.tsx";
