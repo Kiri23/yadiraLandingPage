b0f5d83bcbeae1a5ef54eab4e78eb8984a9e46bb
kiri23 <christian@opticpower.com>
2024-07-10 08:48:21 -0400

    navbar with tailwind css

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,19 @@
 export default function Navbar() {
   return (
-    <header class="navbar bg-pink py-5">
-      <div class="navbar-start">
-        <a class="btn btn-ghost text-xl">Be In peace</a>
-      </div>
-      <div class="navbar-end">
-        <a class="btn bg-blue text-neutral-content">Schedule a session</a>
-      </div>
+    <header class="flex flex-wrap md:justify-start md:flex-nowrap bg-pink z-50 w-full py-7">
+      <nav className="relative w-full flex flex-wrap md:grid md:grid-cols-12 basis-full items-center px-4 md:px-8 mx-auto">
+        <div class="md:col-span-6">
+          <a class="btn btn-ghost text-xl">Be In peace</a>
+        </div>
+        <div class=" flex items-center md:col-span-6 ms-auto">
+          <button
+            type={"button"}
+            class="py-4 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl bg-white hover:shadow-lg"
+          >
+            Schedule a session
+          </button>
+        </div>
+      </nav>
     </header>
   );
 }
