cae9d7e58634dc8159a22d754462242f14f34876
kiri23 <christian@opticpower.com>
2024-07-10 23:24:32 -0400

    make the logo bigger

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,19 +5,21 @@ export default function Navbar() {
     // 200 - fb cf e8
     // 300 - f9 a8 d4
     // bg-pink-110
-    <header class="flex flex-wrap sticky top-0 md:relative md:justify-start md:flex-nowrap bg-pink-110 z-50 w-full">
-      <nav className="relative w-full flex flex-wrap md:grid md:grid-cols-12 basis-full items-center px-4 md:px-8 mx-auto">
+
+    // position relative - position the log
+    <header class="flex flex-wrap sticky top-0 md:relative md:justify-start md:flex-nowrap z-50 bg-pink-110 w-full">
+      <nav className="w-full flex flex-wrap h-1 sm:h-20 pb-20 md:grid md:grid-cols-12 basis-full items-center px-4 md:px-8 mx-auto">
         <div class="md:col-span-6">
           <img
             src="beInPeaceLogo.svg"
             alt="Logo"
-            className="w-28 h-28"
+            className="w-28 h-28 sm:w-48 sm:h-20 object-cover"
           />
         </div>
         <div class=" flex items-center md:col-span-6 ms-auto">
           <button
             type={"button"}
-            class="py-4 px-4 inline-flex items-center gap-x-2 text-sm text-white font-medium rounded-xl bg-blueBrand hover:shadow-lg"
+            class="py-1 sm:py-2 px-4 inline-flex items-center gap-x-2 text-sm text-white font-medium rounded-xl bg-blueBrand hover:shadow-lg"
           >
             Schedule a session
           </button>
