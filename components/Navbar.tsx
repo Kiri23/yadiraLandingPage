0a95294c645d5fcadb54a2360c5738ca50efecc7
kiri23 <christian@opticpower.com>
2024-07-11 00:14:52 -0400

    adjust size of logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,11 +6,11 @@ export default function Navbar() {
         <img
           src="beInPeaceLogo.svg"
           alt="Logo"
-          className="w-32 h-44 sm:w-52 sm:h-52 sm:object-cover"
+          className="w-40 h-44 sm:w-52 sm:h-52 sm:object-cover"
         />
         <button
           type={"button"}
-          class="items-end py-1 sm:py-2 px-4 text-sm text-white font-medium rounded-xl bg-blueBrand hover:shadow-lg"
+          class="items-end py-2 sm:py-3 px-4 text-sm text-white font-medium rounded-xl bg-blueBrand hover:shadow-lg"
         >
           Schedule a session
         </button>
